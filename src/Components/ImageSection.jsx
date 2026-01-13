a0df05206b8f15c98f9964afba65c6552899cba5
SS40852850 <german.garcia@rcpo.com.ar>
2026-01-13 17:00:49 -0300

    SMS added | age and description changed | resume updated

--- src/Components/ImageSection.jsx
+++ src/Components/ImageSection.jsx
@@ -14,7 +14,7 @@ function ImageSection() {
           I am <span>Germán García</span>
         </h4>
         <p className="paragraph">
-          Hi! My name is Germán, and I'm a Software Developer and Violinist. I have two years of experience working with React and Node, and I'm currently part of the team at Propital. My primary stack is React and Node, but I also have experience with PHP, Golang and Python. I created this portfolio to showcase my work and the projects I've been involved in. Feel free to reach out to me!
+          Hi! My name is Germán, and I'm a Software Developer and Violinist. I have three years of experience working with React and Node, and I'm currently part of the team at SMS Sudamérica. My primary stack is React and Node, but I also have experience with PHP, Golang and Python. I created this portfolio to showcase my work and the projects I've been involved in. Feel free to reach out to me!
         </p>
         <div className="about-info">
           <div className="info-title">
@@ -25,13 +25,13 @@ function ImageSection() {
           </div>
           <div className="info">
             <p>Germán García</p>
-            <p>26</p>
+            <p>28</p>
             <p>Buenos Aires, Argentina </p>
             <p>Spanish (Native), English (Proficient: C2)</p>
           </div>
         </div>
         <a
-          href="https://drive.google.com/file/d/1jeHSaAWK5A3e9846IdNRsxGDUrPddvwl/view?usp=sharing"
+          href="https://drive.google.com/file/d/1aX6AMOvjSzdu_GgnBdu_r4kpeiavZ45r/view?usp=sharing"
           target="_blank" rel="noreferrer"
         >
           <PrimaryButton title={"CV"} />
